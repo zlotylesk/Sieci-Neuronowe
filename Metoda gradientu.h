78a51a54db0cf70b632cd3a89d324d50cfcc7b0b
Leszek Koziatek <zlotylesk@o2.pl>
2015-05-24 23:04:57 +0200

    Zadanie 4. Metoda gradientu

--- Metoda gradientu.h
+++ Metoda gradientu.h
@@ -6,6 +6,11 @@
 #define SIECI_NEURONOWE_METODA_GRADIENTU_H
 //libraries
 #include "iostream"
+#include "stdio.h"
+#include "vector"
+#include "math.h"
+#include "time.h"
+#include "stdlib.h"
 using namespace std;
 //funcions
 void introSNE04();
